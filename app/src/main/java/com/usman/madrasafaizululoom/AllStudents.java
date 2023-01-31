5e22668f918edc12d7567a1ffc01a3eb4af1fade
Usman Monir <usmanmunir026@gmail.comr>
2023-01-31 18:05:28 +0500

    All Students Views Added

---
+++ app/src/main/java/com/usman/madrasafaizululoom/AllStudents.java
@@ -0,0 +1,14 @@
+package com.usman.madrasafaizululoom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllStudents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_students);
+    }
+}
